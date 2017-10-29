3a176609f4bf76953ba3eea250f957df9ce0cdda
Nanami Yamamoto <nanamin0634@gmail.com>
2017-10-29 17:23:11 +0900

    add
    高精度タイマークラス"HighPrecisionTimer"を追加

---
+++ HighPrecisionTimer.cpp
@@ -0,0 +1,128 @@
+#include "HighPrecisionTimer.h"
+
+HighPrecisionTimer::HighPrecisionTimer() {
+	llStartCount.QuadPart = 0;
+	llPauseStCount.QuadPart = 0;
+	llFrequency.QuadPart = 0;
+
+	bIsPaused = true;
+	bIsReset = true;
+	dTime = 0.0;
+
+	// 周波数を取得
+	if (QueryPerformanceFrequency(&llFrequency)) {
+		bIsHighPrecValid = true;
+	}
+	else {
+		/*
+		高精度タイマーが使用できない場合、timeGetTime関数で代用します。
+		その際、精度を1msで取得するので周波数を1000Hzに設定します。
+		*/
+
+		llFrequency.QuadPart = 1000LL;
+	}
+
+}
+
+
+HighPrecisionTimer::~HighPrecisionTimer() {
+}
+
+
+void HighPrecisionTimer::Start() {
+
+	if (bIsPaused) {
+		LARGE_INTEGER now;
+
+		// 現在のカウントを取得
+		if (bIsHighPrecValid) {
+			QueryPerformanceCounter(&now);
+		}
+		else {
+			timeBeginPeriod(1);
+			now.QuadPart = (long long)timeGetTime();
+			timeEndPeriod(1);
+		}
+
+		// タイマー開始の時間を変更
+		if (bIsReset) {
+			llStartCount.QuadPart = now.QuadPart;
+		}
+		else {
+			// 一時停止した時間分だけずらす
+			llStartCount.QuadPart += now.QuadPart - llPauseStCount.QuadPart;
+		}
+		
+
+		bIsPaused = false;
+		bIsReset = false;
+	}
+
+}
+
+
+void HighPrecisionTimer::Pause() {
+
+	if (bIsPaused) {
+		/*
+		既に一時停止中の場合は何もしません
+		*/
+	}
+	else {
+
+		// 一時停止開始時のカウントを取得
+		if (bIsHighPrecValid) {
+			QueryPerformanceCounter(&llPauseStCount);
+		}
+		else {
+			timeBeginPeriod(1);
+			llPauseStCount.QuadPart = (long long)timeGetTime();
+			timeEndPeriod(1);
+		}
+
+		// 一時停止時のタイムを保存
+		dTime = (double)(llPauseStCount.QuadPart - llStartCount.QuadPart)/llFrequency.QuadPart;
+
+	}
+
+	bIsPaused = true;
+
+}
+
+
+void HighPrecisionTimer::Reset() {
+
+	if (bIsPaused) {
+		dTime = 0.0;
+
+		bIsReset = true;
+	}
+	else {
+		/*
+		一時停止中でないと動作しません
+		*/
+	}
+
+}
+
+
+double HighPrecisionTimer::GetTime() {
+
+	if (bIsPaused == false) {
+
+		// タイムを更新
+		LARGE_INTEGER now;
+		if (bIsHighPrecValid) {
+			QueryPerformanceCounter(&now);
+		}
+		else {
+			timeBeginPeriod(1);
+			now.QuadPart = timeGetTime();
+			timeEndPeriod(1);
+		}
+
+		dTime = (double)(now.QuadPart - llStartCount.QuadPart)/llFrequency.QuadPart;
+	}
+
+	return dTime;
+}
---
+++ HighPrecisionTimer.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+/*
+HighPrecisionTimerクラス
+
+概要:時間を高精度(1ms以下)で測定することができるクラスです。
+
+実際のストップウォッチ同じ挙動をするように、プログラムを設計しました。
+関数の呼び出しも、ストップウォッチと同じように行って下さい。
+
+
+作成者:山本七海
+作成日時:2017/10/29
+参考:http://www.charatsoft.com/develop/otogema/
+
+*/
+
+
+
+#include <Windows.h>
+
+class HighPrecisionTimer {
+private:
+	LARGE_INTEGER llFrequency;
+
+	LARGE_INTEGER llStartCount;
+	LARGE_INTEGER llPauseStCount;
+	
+	double dTime;
+
+	bool bIsHighPrecValid;
+	bool bIsPaused;
+	bool bIsReset;
+
+
+public:
+	HighPrecisionTimer();
+	~HighPrecisionTimer();
+
+	// 高精度タイマーが有効かどうかを返す
+	bool IsHighPrecisionValid() { return bIsHighPrecValid; }
+
+	// タイマーをスタート
+	void Start();
+
+	// タイマーを一時停止
+	void Pause();
+
+	// タイマーをリセット
+	void Reset();
+
+	// 時間を取得
+	double GetTime();
+};
